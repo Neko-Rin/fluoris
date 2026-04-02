544a9eaf01d1802939fa685393e339a850af60cb
Neko-Rin <zhengeddie08@gmail.com>
2026-04-01 22:28:33 -0400

    feat: /verify MC User name validation

--- src/verify.rs
+++ src/verify.rs
@@ -5,16 +5,22 @@ use poise::{CreateReply, serenity_prelude as serenity};
 #[poise::command(slash_command)]
 pub async fn verify(
     ctx: Context<'_>,
-    #[description = "Selected user"] user: Option<serenity::User>,
+    #[description = "MC User"] name: String,
 ) -> Result<(), Error> {
-    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let u = ctx.author();
     let Some(g) = ctx.guild_id() else {
         let _ = ctx.say("You can't use this in dms").await;
         return Ok(());
     };
+
+    //Put the role you are checking for here, speficially the role id
     let r = serenity::RoleId::new(1482837780932460788);
     let role = u.has_role(ctx.http(), g, r).await.unwrap_or(false);
     let mut response = format!("You are not verified");
+
+    // checks if minecraft username is vaild
+    let mc_api_format = format!("https://api.mojang.com/users/profiles/minecraft/{}", name);
+    let is_username = reqwest::get(mc_api_format).await?.text().await?;
 
     if role {
         response = format!("You are verified");
